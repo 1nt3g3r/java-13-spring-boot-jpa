2273df797c2b70b8ae3b1749468c4976d18adcc5
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2023-11-23 21:40:30 +0200

    Added base security

--- src/main/java/com/goit/java13/mvc/feature/chocholate/ChocolateController.java
+++ src/main/java/com/goit/java13/mvc/feature/chocholate/ChocolateController.java
@@ -1,8 +1,15 @@
 package com.goit.java13.mvc.feature.chocholate;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+import java.util.Collection;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -11,8 +18,22 @@ import java.util.List;
 public class ChocolateController {
     private final ChocolateService chocolateService;
 
+    @PreAuthorize("hasRole('SUPERUSER')")
     @GetMapping("/list")
     public List<ChocolateDto> list() {
+        ///Отримуємо дані про аутентифікованого юзера
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        String username = authentication.getName();
+        Object principal = authentication.getPrincipal();
+        System.out.println("username = " + username);
+        System.out.println("principal = " + principal);
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        for (GrantedAuthority authority : authorities) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
+
         return ChocolateDto.from(chocolateService.findAll());
     }
 
